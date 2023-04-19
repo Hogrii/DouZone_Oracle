81a1607bdd802c6ec201b3e3e28b57398136654c
LJH <hosboy93@naver.com>
2023-04-19 14:47:07 +0900

    ~서브쿼리 예제

--- 관리자.sql
+++ 관리자.sql
@@ -1299,6 +1299,96 @@ on e.deptno = e1.deptno
 where e.sal > e1.avgsal;
 
 
+--1. 'SMITH'보다 월급을 많이 받는 사원들의 이름과 월급을 출력하라.
+select sal from emp where ename = 'SMITH';
+select ename, sal from emp where sal > (select sal from emp where ename = 'SMITH');
+
+--2. 10번 부서의 사원들과 같은 월급을 받는 사원들의 이름, 월급,
+-- 부서번호를 출력하라.
+select sal from emp where deptno = 10;
+select ename, sal, deptno from emp where sal in (select sal from emp where deptno = 10);
+
+--3. 'BLAKE'와 같은 부서에 있는 사원들의 이름과 고용일을 뽑는데
+-- 'BLAKE'는 빼고 출력하라
+select deptno from emp where ename = 'BLAKE';
+select ename, hiredate from emp where deptno = (select deptno from emp where ename = 'BLAKE') and ename != 'BLAKE';
+
+--4. 평균급여보다 많은 급여를 받는 사원들의 사원번호, 이름, 월급을
+-- 출력하되, 월급이 높은 사람 순으로 출력하라.
+select avg(sal) from emp;
+select empno, ename, sal 
+from emp 
+where sal > (select avg(sal) from emp)
+order by sal desc;
+
+--5. 이름에 'T'를 포함하고 있는 사원들과 같은 부서에서 근무하고
+-- 있는 사원의 사원번호와 이름을 출력하라.
+select deptno from emp where ename like '%T%';
+select empno, ename 
+from emp
+where deptno in (select deptno from emp where ename like '%T%');
+
+--6. 30번 부서에 있는 사원들 중에서 가장 많은 월급을 받는 사원보다
+-- 많은 월급을 받는 사원들의 이름, 부서번호, 월급을 출력하라.
+--(단, ALL(and) 또는 ANY(or) 연산자를 사용할 것)
+select max(sal) from emp where deptno = 30;
+select ename, deptno, sal 
+from emp 
+where sal > ALL(select max(sal) from emp where deptno = 30);
+
+--7. 'DALLAS'에서 근무하고 있는 사원과 같은 부서에서 일하는 사원의
+-- 이름, 부서번호, 직업을 출력하라.
+select d.deptno from emp e join dept d on e.deptno = d.deptno where d.loc = 'DALLAS';
+select ename, deptno, job 
+from emp
+where deptno in (select d.deptno from emp e join dept d on e.deptno = d.deptno where d.loc = 'DALLAS');
+
+--8. SALES 부서에서 일하는 사원들의 부서번호, 이름, 직업을 출력하라.
+select e.deptno, e.ename, e.job 
+from emp e join dept d on e.deptno = d.deptno 
+where d.dname = 'SALES';
+
+--9. 'KING'에게 보고하는 모든 사원의 이름과 급여를 출력하라
+--king 이 사수인 사람 (mgr 데이터가 king 사번)
+select empno from emp where ename = 'KING';
+select ename, sal 
+from emp
+where mgr = (select empno from emp where ename = 'KING');
+
+--10. 자신의 급여가 평균 급여보다 많고, 이름에 'S'가 들어가는
+-- 사원과 동일한 부서에서 근무하는 모든 사원의 사원번호, 이름,
+-- 급여를 출력하라.
+select avg(sal) from emp;
+select deptno from emp where sal > (select avg(sal) from emp) and ename like '%S%';
+select empno, ename, sal 
+from emp
+where sal > (select avg(sal) from emp)
+    and deptno in (select deptno from emp where ename like '%S%');
+
+--11. 커미션을 받는 사원과 부서번호, 월급이 같은 사원의
+-- 이름, 월급, 부서번호를 출력하라.
+select deptno, sal from emp where comm is not null;
+select ename, sal, deptno
+from emp
+where deptno in (select deptno from emp where comm is not null)
+    and sal in (select sal from emp where comm is not null);
+
+--12. 30번 부서 사원들과 월급과 커미션이 같지 않은
+-- 사원들의 이름, 월급, 커미션을 출력하라.
+select sal, comm from emp where deptno = 30;
+select ename, sal, comm 
+from emp
+where sal not in (select sal from emp where deptno = 30) 
+    and comm not in (select nvl(comm, 0) from emp where deptno = 30 
+    and comm is not null);
+
+
+
+
+
+
+
+
 
 
 
