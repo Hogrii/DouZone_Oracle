7a9eb490c901a2077e0f259b72455b633f9076b0
LJH <hosboy93@naver.com>
2023-04-21 16:03:26 +0900

    rownum~

--- 관리자.sql
+++ 관리자.sql
@@ -2147,6 +2147,225 @@ group by l.city;
 select trunc((months_between('2010-01-01', hire_date)/12), 0) as 근속년수
 from employees;
 --------------------------------------------------------------------------------
+/*
+Sequence 특징
+1) 자동적으로 유일 번호를 생성합니다.
+2) 공유 가능한 객체(테이블간)
+3) 주로 기본 키 값을 생성하기 위해 사용됩니다.
+4) 어플리케이션 코드를 대체합니다.(로직을 만들 필요가 없다)
+5) 메모리에 CACHE 되면 SEQUENCE 값을 액세스 하는 효율성을 향상시킵니다.
+
+CREATE SEQUENCE sequence_name
+[INCREMENT BY n]
+[START WITH n]
+[{MAXVALUE n | NOMAXVALUE}]
+[{MINVALUE n | NOMINVALUE}]
+[{CYCLE | NOCYCLE}]
+[{CACHE | NOCACHE}];
+
+sequence_name : SEQUENCE 의 이름입니다.
+INCREMENT BY n : 정수 값인 n 으로 SEQUENCE 번호 사이의 간격을 지정. 이 절이 생략되면 SEQUENCE 는 1 씩 증가.
+START WITH n : 생성하기 위해 첫번째 SEQUENCE 를 지정. 이 절이 생략되면 SEQUENCE 는 1 로 시작.
+MAXVALUE n : SEQUENCE 를 생성할 수 있는 최대 값을 지정.
+NOMAXVALUE : 오름차순용 10^27 최대값과 내림차순용-1 의 최소값을 지정.
+MINVALUE n : 최소 SEQUENCE 값을 지정.
+NOMINVALUE : 오름차순용 1 과 내림차순용-(10^26)의 최소값을 지정.
+CYCLE | NOCYCLE : 최대 또는 최소값에 도달한 후에 계속 값을 생성할 지의 여부를 지정. NOCYCLE 이 디폴트.
+CACHE | NOCACHE : 얼마나 많은 값이 메모리에 오라클 서버가 미리 할당하고 유지하는가를 지정. 디폴트로 오라클 서버는 20 을 CACHE
+ */
+
+desc board;
+drop table board;
+
+create table board(
+    boardid number constraint pk_board_boardid primary key,
+    title nvarchar2(50)
+);
+
+select * from user_constraints where lower(table_name) = 'board';
+-- pk : not null, unique, index(검색 속도 향상)
+
+-- 게시판 글쓰기 작업
+insert into board(boardid, title) values(1, '처음글');
+insert into board(boardid, title) values(2, '두번째');
+
+select * from board;
+rollback;
+-- 처음 글을 쓰면 글 번호가 1번 .. 그 다음글부터는 순차적인 증가값 .. 2번, 3번, ..
+-- 어떤 논리?
+select count(boardid) + 1 from board;
+
+insert into board(boardid, title) values((select count(boardid) + 1 from board), '내용1');
+insert into board(boardid, title) values((select count(boardid) + 1 from board), '내용2');
+insert into board(boardid, title) values((select count(boardid) + 1 from board), '내용3');
+
+select * from board;
+-- 게시글 삭제, 수정 ..
+commit;
+
+delete from board where boardid = 1;
+select * from board;
+commit;
+
+insert into board(boardid, title) values((select count(boardid) + 1 from board), '새글');
+-- ORA-00001: unique constraint (KOSA.PK_BOARD_BOARDID) violated
+
+-- 데이터가 지워져도(삭제되어도) 문제가 없는 순번을 가지고 싶다
+insert into board(boardid, title) values((select nvl(max(boardid),0) + 1 from board), '새글');
+select * from board;
+
+-- 시퀀스 생성하기(순번 만들기) : 객체(create ...) : 순차적인 번호를 생성하는 객체
+create sequence board_num;
+
+-- 순번
+select board_num.nextval from dual; -- nextval : 채번(번호표 뽑기)
+select board_num.currval from dual; -- currval : 현재까지 채번한 번호 확인
+
+-- 공유(객체) >> 하나의 테이블이 아니라 여러개의 테이블을 사용
+
+-- A 테이블,       B 테이블
+-- (insert >> 1   (insert >> 2, insert >> 3)
+-- (insert >> 4
+
+create table kboard(
+    num number constraint pk_kboard_num primary key,
+    title nvarchar2(20)
+);
+create sequence kboard_num;
+insert into kboard(num, title) values(kboard_num.nextval, '처음');
+insert into kboard(num, title) values(kboard_num.nextval, '두번째');
+insert into kboard(num, title) values(kboard_num.nextval, '세번째');
+select * from kboard;
+--------------------------------------------------------------------------------
+/*
+게시판
+공지사항, 자유게시판, 답변형게시판 ....
+공지사항 1, 2, 3     >> 공지사항 시퀀스
+자유게시판 1, 2, 3   >> 자유게시판 시퀀스
+답변형게시판 1, 2, 3 >> 답변형게시판 시퀀스
+---------------------------------------
+공지사항 1, 6
+자유게시판 2, 3
+답변형게시판 4, 5
+>> 시퀀스를 1개만 가지고 3개 테이블에서 나눠 사용(공유 객체)
+
+TIP)
+sequence >> 모든 DB에 다 있을까..?
+오라클 (O)
+MS-sql (O) >> 2012 Ver~
+My-sql (X)
+MariaDB(엔진(MS-sql)) (O) : https://mariadb.com/kb/ko/sequence-overview/
+PostgreSQL (O)
+
+-- 순번 생성(테이블에 종속적으로)
+MS-sql
+create table board(boardnum int identity(1, 1) .. title
+insert into board(title) values('제목'); >> boardnum >> 1이 자동으로 들어간다
+
+My-sql
+create table board(boardnum int auto_increament, .. title
+insert into board(title) values('제목'); >> boardnum >> 1이 자동으로 들어간다
+ */
+-- 옵션
+create sequence seq_num start with 10 increment by 2;
+
+select seq_num.nextval from dual;
+select seq_num.currval from dual;
+
+-- 순번
+-- 게시판 처음 ... 데이터 가져올 때
+-- 쿼리문
+-- num >> 1, 2, 3, .., 1000, .., 10000
+-- 가장 나중에 쓴 글(최신글)
+-- select * from board order by num desc
+
+-- rownum : 의사 컬럼 : 실제 물리적으로 존재하는 컬럼이 아니고 논리적 존재(create table (x) 사용 안돼요)
+-- rownum : 실제로 테이블에 컬럼으로 존재하지 않지만 내부적으로 행 번호를 부여하는 컬럼
+
+select * from emp;
+select rownum, empno, ename, sal from emp;
+
+select empno, ename, sal from emp order by sal; 
+-- select가 먼저 실행되고 order by가 실행되기 때문에 뒤죽박죽이가 된다
+-- 실행 순서 >> from >> select >> order by
+
+select rownum, e.*
+from (
+        select empno, ename, sal 
+        from emp 
+        order by sal
+     ) e;
+-- rownum은 select한 결과에 붙인다
+
+-- Top-n 쿼리(기준이 되는 데이터 순으로 정렬시키고 상위 n개를 가져오는 방법) >> Oracle에는 없음..
+-- MS-sql : select top 10, * from emp order by sal desc;
+-- Oracle에서는 rownum을 이용해서 Top-n 쿼리를 대체해야 한다
+-- 1. 정렬의 기준 설정하기(선행)
+-- 2. 정렬된 기준에 rownum을 붙이고 .. 데이터 추출
+
+-- 급여를 많이 받는 순으로 정렬된 데이터에 rownum >> 순번을 가짐
+select rownum, e.*
+from (
+        select empno, ename, sal 
+        from emp 
+        order by sal desc
+     ) e;
+
+-- 급여를 많이 받는 사원 5명
+select *
+from (
+        select rownum as num, e.*
+        from (
+                select empno, ename, sal 
+                from emp 
+                order by sal desc
+             ) e
+     ) n
+where num <= 5;
+-- Today Point !!
+-- ***** 대용량 데이터에서 페이징의 원리
+-- 페이징 >> between A and B 
+--------------------------------------------------------------------------------
+-- 기업(10만 ~ 1억건)
+-- 게시판(게시글 10만건)
+-- select * from board >> 10만건을 조회
+-- 10만건을 나누어서 10건, 20건씩 봐야 한다
+/*
+totaldata = 100
+pagesize = 10 >> 한 화면에 보여지는 데이터 row수를 10건씩 할거야
+page 개수 >> 10개
+
+[1][2][3][4][5][6][7][8][9][10]
+<a href='page.jsp?page=1'>1</a>
+1page 클릭 >> 1~10까지 글 : DB 쿼리 : select num between 1 and 10
+2page 클릭 >> 11~20까지 글 : DB 쿼리 : select num between 11 and 20
+
+1. rownum
+2. between
+ */
+
+-- HR 계정 이동
+show user;
+select * from employees; -- 107건
+
+-- 사원 번호가 낮은 순으로 정렬한 기준을 통해서 .. 41부터 50번째 데이터를 출력하세요
+select *
+from(
+        select rownum as num, e.*
+        from (
+                select *
+                from employees
+                order by employee_id
+             ) e
+     ) n
+where num between 41 and 50;
+-- 게시판 만들기 할때 사용합니다
+-- servlet/jsp
+--------------------------------------------------------------------------------
+-- 분석함수(조금씩) -- 개념(index) -- PL-sql 진도 조금씩 할게요
+--------------------------------------------------------------------------------
+-- 담주 JDBC(JAVA) - MariaDB세팅(기본작업)
+
 
 
 
